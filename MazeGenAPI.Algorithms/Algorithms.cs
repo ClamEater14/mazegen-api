205c6f38f1a8d8ca4b6008d8c405ecbc650ec68b
Caleb Lam <caleb.lam@hotmail.com>
2022-06-17 21:38:01 -0400

    Implement Random Prim algorithm

--- MazeGenAPI.Algorithms/Algorithms.cs
+++ MazeGenAPI.Algorithms/Algorithms.cs
@@ -25,5 +25,101 @@ namespace MazeGenAPI.Generators
                 _ => throw new ArgumentException("algorithm not found!"),
             };
         }
+
+        internal static void ClearStartAndEndWalls(char[][] maze,
+                                           int width,
+                                           int height,
+                                           int startY,
+                                           int endY,
+                                           char wallSymbol,
+                                           char floorSymbol)
+        {
+            Random random = new();
+            List<(int, int)> wallsToClear = new(3); // positions of walls around start (then end) for potential clearing
+            bool shouldClear = true;
+
+            if (maze[startY][1] == wallSymbol)
+            {
+                wallsToClear.Add((startY, 1));
+            }
+            else
+            {
+                shouldClear = false;
+            }
+
+            if (startY > 0)
+            {
+                if (maze[startY - 1][0] == wallSymbol)
+                {
+                    wallsToClear.Add((startY - 1, 0));
+                }
+                else
+                {
+                    shouldClear = false;
+                }
+            }
+
+            if (startY < height - 1)
+            {
+                if (startY < height - 1 && maze[startY + 1][0] == wallSymbol)
+                {
+                    wallsToClear.Add((startY + 1, 0));
+                }
+                else
+                {
+                    shouldClear = false;
+                }
+            }
+
+            // Destroy a wall surrounding start
+            if (shouldClear)
+            {
+                var (toClearRow, toClearCol) = wallsToClear[random.Next(0, wallsToClear.Count)];
+                maze[toClearRow][toClearCol] = floorSymbol;
+            }
+
+            wallsToClear.Clear();
+            shouldClear = true;
+
+            if (maze[endY][width - 2] == wallSymbol)
+            {
+                wallsToClear.Add((endY, width - 2));
+            }
+            else
+            {
+                shouldClear = false;
+            }
+
+            if (endY > 0)
+            {
+                if (maze[endY - 1][width - 1] == wallSymbol)
+                {
+                    wallsToClear.Add((endY - 1, width - 1));
+                }
+                else
+                {
+                    shouldClear = false;
+                }
+            }
+
+            if (endY < height - 1)
+            {
+                if (maze[endY + 1][width - 1] == wallSymbol)
+                {
+                    wallsToClear.Add((endY + 1, width - 1));
+                }
+                else
+                {
+                    shouldClear = false;
+                }
+            }
+
+            // Destroy a wall surrounding end
+            if (shouldClear)
+            {
+                var (toClearRow, toClearCol) = wallsToClear[random.Next(0, wallsToClear.Count)];
+                maze[toClearRow][toClearCol] = floorSymbol;
+            }
+        }
     }
 }
--- MazeGenAPI.Algorithms/PrimsAlgorithm.cs
+++ MazeGenAPI.Algorithms/PrimsAlgorithm.cs
@@ -9,27 +9,94 @@
                                      char wallSymbol,
                                      char floorSymbol)
         {
+            Random random = new();
             char[][] result = new char[height][];
-            HashSet<(int, int)> walls = new();
+            List<(int, int, int, int)> frontiers = new(); // (y0, x0, y1, x1): set of two consecutive points on a maze (inverted for array indexing)
+            List<int> startYs = new();
+            List<int> endYs = new();
+            int x = random.Next(0, checked((int)width));
+            int y = random.Next(0, checked((int)height));
+            frontiers.Add((y, x, y, x));
+
             for (int i = 0; i < height; i++)
             {
-                result[i] = new string(floorSymbol,
+                result[i] = new string(wallSymbol,
                                        checked((int)width)).ToCharArray();
+            }
 
-                for (int j = 0; j < width; j++)
+            while (frontiers.Count > 0)
+            {
+                int i = random.Next(frontiers.Count);
+                var (y0, x0, y1, x1) = frontiers[i];
+                frontiers.RemoveAt(i);
+
+                x = x1;
+                y = y1;
+
+                if (result[y][x] == wallSymbol)
                 {
-                    //result[i][j] = ((i + j) % 2 == 0 && i % 2 == 0) ? floorSymbol : wallSymbol;
-                    if ((i + j) % 2 == 0 && i % 2 == 0)
+                    result[y0][x0] = result[y][x] = floorSymbol;
+
+                    if (x0 == 0)
                     {
-                        result[i][j] = floorSymbol;
-                    } else
+                        startYs.Add(y0);
+                    }
+
+                    if (x0 == width - 1)
                     {
-                        result[i][j] = wallSymbol;
-                        walls.Add((i, j));
+                        endYs.Add(y0);
+                    }
+
+                    if (x == 0)
+                    {
+                        startYs.Add(y);
+                    }
+
+                    if (x == width - 1)
+                    {
+                        endYs.Add(y);
+                    }
+
+                    if (x >= 2 && result[y][x - 2] == wallSymbol)
+                    {
+                        frontiers.Add((y, x - 1, y, x - 2));
+                    }
+
+                    if (y >= 2 && result[y - 2][x] == wallSymbol)
+                    {
+                        frontiers.Add((y - 1, x, y - 2, x));
+                    }
+
+                    if (x < width - 2 && result[y][x + 2] == wallSymbol)
+                    {
+                        frontiers.Add((y, x + 1, y, x + 2));
+                    }
+
+                    if (y < height - 2 && result[y + 2][x] == wallSymbol)
+                    {
+                        frontiers.Add((y + 1, x, y + 2, x));
                     }
                 }
-
             }
+
+            int startY = startYs.Count > 0 ? startYs[random.Next(0, startYs.Count)] : random.Next(0, checked((int)width));
+
+            int endYIndex = 0;
+            int endY = endYs.Count > 0 ? endYs[endYIndex = random.Next(0, endYs.Count)] : random.Next(0, checked((int)height));
+
+            while (startY == endY)
+            {
+                if (endYs.Count > 0)
+                {
+                    endYs.RemoveAt(endYIndex);
+                }
+
+                endY = endYs.Count > 0 ? endYs[endYIndex = random.Next(0, endYs.Count)] : random.Next(0, checked((int)height));
+            }
+
+            result[startY][0] = startSymbol;
+            result[endY][width - 1] = endSymbol;
+            Algorithms.ClearStartAndEndWalls(result, checked((int)width), checked((int)height), startY, endY, wallSymbol, floorSymbol);
 
             return result;
         }
